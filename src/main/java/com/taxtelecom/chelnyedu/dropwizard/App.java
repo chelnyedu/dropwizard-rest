69a319b3ece58f5859573fcebcb6d97df2cdcb0a
ilya746 <krutikov_ilya@mail.ru>
2017-07-05 13:42:01 +0300

    create Rest Resources

--- src/main/java/com/taxtelecom/chelnyedu/dropwizard/App.java
+++ src/main/java/com/taxtelecom/chelnyedu/dropwizard/App.java
@@ -1,5 +1,6 @@
 package com.taxtelecom.chelnyedu.dropwizard;
 
+import com.taxtelecom.chelnyedu.dropwizard.resources.ContactResource;
 import org.slf4j.LoggerFactory;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
@@ -10,12 +11,12 @@ private static final org.slf4j.Logger logger = LoggerFactory.getLogger(App.class
     @Override
     public void initialize(Bootstrap<ApplicationConfiguration> b) {}
     @Override
-    public void run(ApplicationConfiguration c, Environment e) throws
-            Exception {
+    public void run(ApplicationConfiguration c, Environment e) throws Exception {
         logger.info("Method App#run() called");
         for (int i=0; i < c.getMessageRepetitions(); i++) {
         	System.out.println(c.getMessage());
         }
+        e.jersey().register(new ContactResource());
     }
 
     public static void main( String[] args ) throws Exception
