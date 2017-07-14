f9b15ada3a2342132ae0f0e561f1f296ea1c2bb4
asikabi <adamblein@gmail.com>
2017-07-14 10:51:30 +0300

    Client was created!

--- src/main/java/com/taxtelecom/chelnyedu/dropwizard/App.java
+++ src/main/java/com/taxtelecom/chelnyedu/dropwizard/App.java
@@ -1,6 +1,8 @@
 package com.taxtelecom.chelnyedu.dropwizard;
 
+import com.sun.jersey.api.client.Client;
 import com.taxtelecom.chelnyedu.dropwizard.dao.ContactDAO;
+import com.taxtelecom.chelnyedu.dropwizard.resources.ClientResources;
 import com.taxtelecom.chelnyedu.dropwizard.resources.ContactResources;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.jdbi.DBIFactory;
@@ -8,6 +10,7 @@ import io.dropwizard.migrations.MigrationsBundle;
 import org.skife.jdbi.v2.DBI;
 import org.slf4j.LoggerFactory;
 import io.dropwizard.Application;
+import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.setup.Bootstrap;
 
@@ -29,10 +32,12 @@ private static final org.slf4j.Logger logger = LoggerFactory.getLogger(App.class
         for (int i=0; i < c.getMessageRepetitions(); i++) {
         	System.out.println(c.getMessage());
         }
-
         final DBIFactory factory = new DBIFactory();
         final DBI jdbi = factory.build(e,c.getDataSourceFactory(), "myPostgres");
         e.jersey().register(new ContactResources(jdbi.onDemand(ContactDAO.class), e.getValidator()));
+        
+        final Client client = new JerseyClientBuilder(e).build("REST Client");
+        e.jersey().register(new ClientResources(client));
     }
 
     public static void main( String[] args ) throws Exception
